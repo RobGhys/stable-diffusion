458918471b4e573c55c951f6725377c4dfea8172
Robin Ghyselinck <rghyselinck@gmail.com>
2024-11-27 16:42:22 +0100

    fixed bugs

--- stable-diffusion/model/ddpm.py
+++ stable-diffusion/model/ddpm.py
@@ -4,10 +4,9 @@ import numpy as np
 class DDPMSampler:
     def __init__(self, generator: torch.Generator, num_training_steps=1000,
                  beta_start: float=0.00085, beta_end: float= 0.0120):
-        self.start_step = None
         self.betas = torch.linspace(beta_start ** 0.5, beta_end ** 0.5, num_training_steps, dtype=torch.float32) ** 2
         self.alphas = 1.0 - self.betas
-        self.alphas_cumprod = torch.cumprod(self.alphas, 0) # [alpha_0, alpha_0 * alpha_1, alpha_0 * alpha_1 * alpha_2, ...]
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0) # [alpha_0, alpha_0 * alpha_1, alpha_0 * alpha_1 * alpha_2, ...]
         self.one = torch.tensor(1.0)
 
         self.generator = generator
